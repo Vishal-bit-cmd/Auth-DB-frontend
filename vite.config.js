fd2105b2db63e02bd44a57409052a5050b51de4c
Vishal-bit-cmd <vishalsuryask@gmail.com>
2025-10-16 17:04:40 +0530

    third commit

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,6 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: path.resolve(__dirname, '../backend/dist'), // build frontend into backend/dist
-    emptyOutDir: true,
-  },
-  server: {
-    port: 5173, // for local dev
-    proxy: {
-      '/api': 'http://localhost:5000', // backend API proxy for dev
-    },
-  },
-})
+build: {
+    outDir: 'public',
+  }})
